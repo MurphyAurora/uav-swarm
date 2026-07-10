ecb8b59e7b310fe809fd1e0b55a108889f95c182
Zhang Xinzhu <2025140045@bupt.cn>
2026-07-10 22:50:50 +0800

    refactor: export planner and benchmark interfaces

--- src/xtd2_mission/xtd2_mission/planner_framework/__init__.py
+++ src/xtd2_mission/xtd2_mission/planner_framework/__init__.py
@@ -1,5 +1,12 @@
 """Lightweight EGO-like planner framework for XTDrone2 missions."""
 
+from .benchmark_api import (
+    BenchmarkOutput,
+    BenchmarkStep,
+    BenchmarkSystem,
+    NativePlannerSystemAdapter,
+    ScenarioContract,
+)
 from .common import (
     CandidateEvaluation,
     CandidateTrajectory,
@@ -12,23 +19,33 @@ from .common import (
     TrajectoryPoint,
     Vec3,
 )
+from .planner_api import EgoLikePlannerAdapter, PlannerBase, PlannerRequest, PlannerResult
 from .planner_core import EgoLikePlannerCore
 from .reference_generator import LocalAStarReferenceGenerator, LocalReference
 from .sampling_mpc_planner import SamplingMPCPlanner
 
 __all__ = [
+    "BenchmarkOutput",
+    "BenchmarkStep",
+    "BenchmarkSystem",
     "CandidateEvaluation",
     "CandidateTrajectory",
+    "EgoLikePlannerAdapter",
     "EgoLikePlannerCore",
     "LocalAStarReferenceGenerator",
     "LocalReference",
+    "NativePlannerSystemAdapter",
     "Obstacle",
     "PerceptionData",
+    "PlannerBase",
     "PlannerCommand",
     "PlannerConfig",
+    "PlannerRequest",
+    "PlannerResult",
     "PlannerState",
     "SafetyReport",
     "SamplingMPCPlanner",
+    "ScenarioContract",
     "TrajectoryPoint",
     "Vec3",
 ]
